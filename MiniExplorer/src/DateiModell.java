ebd9aefcf1fe57bb1f21c86e66392825e2eec545
Bluecrafter35 <mauchb16@new.edvhtl.at>
2018-09-24 11:37:11 +0200

    DateiModell erstellt

---
+++ MiniExplorer/src/DateiModell.java
@@ -0,0 +1,31 @@
+
+import java.util.ArrayList;
+import javax.swing.AbstractListModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Christoph Mautner
+ */
+public class DateiModell extends AbstractListModel
+{
+    private ArrayList<Datei> dateien = new ArrayList<>();
+    
+    @Override
+    public int getSize()
+    {
+        return dateien.size();
+    }
+
+    @Override
+    public Object getElementAt(int i)
+    {
+        return dateien.get(i);
+    }
+    
+}
